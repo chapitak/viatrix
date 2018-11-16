b0b8debb5eef6a2ec950c227e18f0f6cf061e47c
O <chavitak@gmail.com>
2018-11-16 11:24:36 +0900

    [UPDATE] Add disqus

--- frontend/home/src/components/DBBlogPost.vue
+++ frontend/home/src/components/DBBlogPost.vue
@@ -6,9 +6,10 @@
       <span style="float:right">{{ post.createdAt.substring(0,10) }}</span>
       <div v-html = "post.text">
       </div>
-    
-  <!--<div class="postContent" v-html = "post.text"> </div>-->
     </div>
+    <div id="disqus_thread"></div>
+
+                                
   </div>
 
 </template>
@@ -36,6 +37,25 @@ export default {
 
      var md = MarkDownIt()
      this.post.text = md.render(response.data.text)
+
+    //disqus 추가
+      /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+    
+    var disqus_config = function () {
+    this.page.url = "http://jeongkyo.kim/#/DBBlog/5bee1ad493f3cd542cda23ad";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = this.post.id; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://jeongkyo-kim-1.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+
+
       
     })
     .catch(e => {
